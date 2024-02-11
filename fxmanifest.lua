48417a5114a9d6fc0a922029ebe9b28703f240e9
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:13:53 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,24 +1,25 @@
-fx_version "adamant"
-games {"rdr3"}
+fx_version 'cerulean'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'VORP refactored by @blackpegasus' -- original author FRP framework
+description 'A fishing script for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_fishing-lua'
+
 
 shared_scripts {
 	"config.lua",
 	"locale.lua",
 	"languages/*.lua"
 }
-
 client_scripts {
 	'client/client_js.js',
 	'client/client.lua',
 }
-
-server_scripts {
-	'server/server.lua',
-}
+server_script 'server/server.lua',
 
 exports {
-	'GET_TASK_FISHING_DATA',
+    'GET_TASK_FISHING_DATA',
     'SET_TASK_FISHING_DATA',
     'VERTICAL_PROBE'
 }
