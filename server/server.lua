fcf051c6a0516e572ee0a4784089c9d36b7ddd89
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-05-07 01:42:20 +0100

    refactor(server): merge tables
    remove redundant tables

--- server/server.lua
+++ server/server.lua
@@ -3,105 +3,46 @@ local VORPcore = exports.vorp_core:GetCore()
 local T = Translation.Langs[Config.Lang]
 
 local fishEntity = {
-    [`A_C_FISHBLUEGIL_01_MS`]        = "a_c_fishbluegil_01_ms",
-    [`A_C_FISHBLUEGIL_01_SM`]        = "a_c_fishbluegil_01_sm",
-    [`A_C_FISHBULLHEADCAT_01_MS`]    = "a_c_fishbullheadcat_01_ms",
-    [`A_C_FISHBULLHEADCAT_01_SM`]    = "a_c_fishbullheadcat_01_sm",
-    [`A_C_FISHCHAINPICKEREL_01_MS`]  = "a_c_fishchainpickerel_01_ms",
-    [`A_C_FISHCHAINPICKEREL_01_SM`]  = "a_c_fishchainpickerel_01_sm",
-    [`A_C_FISHCHANNELCATFISH_01_LG`] = "a_c_fishchannelcatfish_01_lg",
-    [`A_C_FISHCHANNELCATFISH_01_XL`] = "a_c_fishchannelcatfish_01_xl",
-    [`A_C_FISHLAKESTURGEON_01_LG`]   = "a_c_fishlakesturgeon_01_lg",
-    [`A_C_FISHLARGEMOUTHBASS_01_LG`] = "a_c_fishlargemouthbass_01_lg",
-    [`A_C_FISHLARGEMOUTHBASS_01_MS`] = "a_c_fishlargemouthbass_01_ms",
-    [`A_C_FISHLONGNOSEGAR_01_LG`]    = "a_c_fishlongnosegar_01_lg",
-    [`A_C_FISHMUSKIE_01_LG`]         = "a_c_fishmuskie_01_lg",
-    [`A_C_FISHNORTHERNPIKE_01_LG`]   = "a_c_fishnorthernpike_01_lg",
-    [`A_C_FISHPERCH_01_MS`]          = "a_c_fishperch_01_ms",
-    [`A_C_FISHPERCH_01_SM`]          = "a_c_fishperch_01_sm",
-    [`A_C_FISHRAINBOWTROUT_01_LG`]   = "a_c_fishrainbowtrout_01_lg",
-    [`A_C_FISHRAINBOWTROUT_01_MS`]   = "a_c_fishrainbowtrout_01_ms",
-    [`A_C_FISHREDFINPICKEREL_01_MS`] = "a_c_fishredfinpickerel_01_ms",
-    [`A_C_FISHREDFINPICKEREL_01_SM`] = "a_c_fishredfinpickerel_01_sm",
-    [`A_C_FISHROCKBASS_01_MS`]       = "a_c_fishrockbass_01_ms",
-    [`A_C_FISHROCKBASS_01_SM`]       = "a_c_fishrockbass_01_sm",
-    [`A_C_FISHSALMONSOCKEYE_01_LG`]  = "a_c_fishsalmonsockeye_01_lg",
-    [`A_C_FISHSALMONSOCKEYE_01_ML`]  = "a_c_fishsalmonsockeye_01_ml",
-    [`A_C_FISHSALMONSOCKEYE_01_MS`]  = "a_c_fishsalmonsockeye_01_ms",
-    [`A_C_FISHSMALLMOUTHBASS_01_LG`] = "a_c_fishsmallmouthbass_01_lg",
-    [`A_C_FISHSMALLMOUTHBASS_01_MS`] = "a_c_fishsmallmouthbass_01_ms",
+    [`A_C_FISHBLUEGIL_01_MS`]        = { entity = "a_c_fishbluegil_01_ms", name = FishData.A_C_FISHBLUEGIL_01_MS[1], texture = FishData.A_C_FISHBLUEGIL_01_MS[2] },
+    [`A_C_FISHBLUEGIL_01_SM`]        = { entity = "a_c_fishbluegil_01_sm", name = FishData.A_C_FISHBLUEGIL_01_SM[1], texture = FishData.A_C_FISHBLUEGIL_01_SM[2] },
+    [`A_C_FISHBULLHEADCAT_01_MS`]    = { entity = "a_c_fishbullheadcat_01_ms", name = FishData.A_C_FISHBULLHEADCAT_01_MS[1], texture = FishData.A_C_FISHBULLHEADCAT_01_MS[2] },
+    [`A_C_FISHBULLHEADCAT_01_SM`]    = { entity = "a_c_fishbullheadcat_01_sm", name = FishData.A_C_FISHBULLHEADCAT_01_SM[1], texture = FishData.A_C_FISHBULLHEADCAT_01_SM[2] },
+    [`A_C_FISHCHAINPICKEREL_01_MS`]  = { entity = "a_c_fishchainpickerel_01_ms", name = FishData.A_C_FISHCHAINPICKEREL_01_MS[1], texture = FishData.A_C_FISHCHAINPICKEREL_01_MS[2] },
+    [`A_C_FISHCHAINPICKEREL_01_SM`]  = { entity = "a_c_fishchainpickerel_01_sm", name = FishData.A_C_FISHCHAINPICKEREL_01_SM[1], texture = FishData.A_C_FISHCHAINPICKEREL_01_SM[2] },
+    [`A_C_FISHCHANNELCATFISH_01_LG`] = { entity = "a_c_fishchannelcatfish_01_lg", name = FishData.A_C_FISHCHANNELCATFISH_01_LG[1], texture = FishData.A_C_FISHCHANNELCATFISH_01_LG[2] },
+    [`A_C_FISHCHANNELCATFISH_01_XL`] = { entity = "a_c_fishchannelcatfish_01_xl", name = FishData.A_C_FISHCHANNELCATFISH_01_XL[1], texture = FishData.A_C_FISHCHANNELCATFISH_01_XL[2] },
+    [`A_C_FISHLAKESTURGEON_01_LG`]   = { entity = "a_c_fishlakesturgeon_01_lg", name = FishData.A_C_FISHLAKESTURGEON_01_LG[1], texture = FishData.A_C_FISHLAKESTURGEON_01_LG[2] },
+    [`A_C_FISHLARGEMOUTHBASS_01_LG`] = { entity = "a_c_fishlargemouthbass_01_lg", name = FishData.A_C_FISHLARGEMOUTHBASS_01_LG[1], texture = FishData.A_C_FISHLARGEMOUTHBASS_01_LG[2] },
+    [`A_C_FISHLARGEMOUTHBASS_01_MS`] = { entity = "a_c_fishlargemouthbass_01_ms", name = FishData.A_C_FISHLARGEMOUTHBASS_01_MS[1], texture = FishData.A_C_FISHLARGEMOUTHBASS_01_MS[2] },
+    [`A_C_FISHLONGNOSEGAR_01_LG`]    = { entity = "a_c_fishlongnosegar_01_lg", name = FishData.A_C_FISHLONGNOSEGAR_01_LG[1], texture = FishData.A_C_FISHLONGNOSEGAR_01_LG[2] },
+    [`A_C_FISHMUSKIE_01_LG`]         = { entity = "a_c_fishmuskie_01_lg", name = FishData.A_C_FISHMUSKIE_01_LG[1], texture = FishData.A_C_FISHMUSKIE_01_LG[2] },
+    [`A_C_FISHNORTHERNPIKE_01_LG`]   = { entity = "a_c_fishnorthernpike_01_lg", name = FishData.A_C_FISHNORTHERNPIKE_01_LG[1], texture = FishData.A_C_FISHNORTHERNPIKE_01_LG[2] },
+    [`A_C_FISHPERCH_01_MS`]          = { entity = "a_c_fishperch_01_ms", name = FishData.A_C_FISHPERCH_01_MS[1], texture = FishData.A_C_FISHPERCH_01_MS[2] },
+    [`A_C_FISHPERCH_01_SM`]          = { entity = "a_c_fishperch_01_sm", name = FishData.A_C_FISHPERCH_01_SM[1], texture = FishData.A_C_FISHPERCH_01_SM[2] },
+    [`A_C_FISHRAINBOWTROUT_01_LG`]   = { entity = "a_c_fishrainbowtrout_01_lg", name = FishData.A_C_FISHRAINBOWTROUT_01_LG[1], texture = FishData.A_C_FISHRAINBOWTROUT_01_LG[2] },
+    [`A_C_FISHRAINBOWTROUT_01_MS`]   = { entity = "a_c_fishrainbowtrout_01_ms", name = FishData.A_C_FISHRAINBOWTROUT_01_MS[1], texture = FishData.A_C_FISHRAINBOWTROUT_01_MS[2] },
+    [`A_C_FISHREDFINPICKEREL_01_MS`] = { entity = "a_c_fishredfinpickerel_01_ms", name = FishData.A_C_FISHREDFINPICKEREL_01_MS[1], texture = FishData.A_C_FISHREDFINPICKEREL_01_MS[2] },
+    [`A_C_FISHREDFINPICKEREL_01_SM`] = { entity = "a_c_fishredfinpickerel_01_sm", name = FishData.A_C_FISHREDFINPICKEREL_01_SM[1], texture = FishData.A_C_FISHREDFINPICKEREL_01_SM[2] },
+    [`A_C_FISHROCKBASS_01_MS`]       = { entity = "a_c_fishrockbass_01_ms", name = FishData.A_C_FISHROCKBASS_01_MS[1], texture = FishData.A_C_FISHROCKBASS_01_MS[2] },
+    [`A_C_FISHROCKBASS_01_SM`]       = { entity = "a_c_fishrockbass_01_sm", name = FishData.A_C_FISHROCKBASS_01_SM[1], texture = FishData.A_C_FISHROCKBASS_01_SM[2] },
+    [`A_C_FISHSALMONSOCKEYE_01_LG`]  = { entity = "a_c_fishsalmonsockeye_01_lg", name = FishData.A_C_FISHSALMONSOCKEYE_01_LG[1], texture = FishData.A_C_FISHSALMONSOCKEYE_01_LG[2] },
+    [`A_C_FISHSALMONSOCKEYE_01_ML`]  = { entity = "a_c_fishsalmonsockeye_01_ml", name = FishData.A_C_FISHSALMONSOCKEYE_01_ML[1], texture = FishData.A_C_FISHSALMONSOCKEYE_01_ML[2] },
+    [`A_C_FISHSALMONSOCKEYE_01_MS`]  = { entity = "a_c_fishsalmonsockeye_01_ms", name = FishData.A_C_FISHSALMONSOCKEYE_01_MS[1], texture = FishData.A_C_FISHSALMONSOCKEYE_01_MS[2] },
+    [`A_C_FISHSMALLMOUTHBASS_01_LG`] = { entity = "a_c_fishsmallmouthbass_01_lg", name = FishData.A_C_FISHSMALLMOUTHBASS_01_LG[1], texture = FishData.A_C_FISHSMALLMOUTHBASS_01_LG[2] },
+    [`A_C_FISHSMALLMOUTHBASS_01_MS`] = { entity = "a_c_fishsmallmouthbass_01_ms", name = FishData.A_C_FISHSMALLMOUTHBASS_01_MS[1], texture = FishData.A_C_FISHSMALLMOUTHBASS_01_MS[2] },
 }
 
-local fishNames = {
-    [`A_C_FISHBLUEGIL_01_MS`]        = FishData.A_C_FISHBLUEGIL_01_MS[1],
-    [`A_C_FISHBLUEGIL_01_SM`]        = FishData.A_C_FISHBLUEGIL_01_SM[1],
-    [`A_C_FISHBULLHEADCAT_01_MS`]    = FishData.A_C_FISHBULLHEADCAT_01_MS[1],
-    [`A_C_FISHBULLHEADCAT_01_SM`]    = FishData.A_C_FISHBULLHEADCAT_01_SM[1],
-    [`A_C_FISHCHAINPICKEREL_01_MS`]  = FishData.A_C_FISHCHAINPICKEREL_01_MS[1],
-    [`A_C_FISHCHAINPICKEREL_01_SM`]  = FishData.A_C_FISHCHAINPICKEREL_01_SM[1],
-    [`A_C_FISHCHANNELCATFISH_01_LG`] = FishData.A_C_FISHCHANNELCATFISH_01_LG[1],
-    [`A_C_FISHCHANNELCATFISH_01_XL`] = FishData.A_C_FISHCHANNELCATFISH_01_XL[1],
-    [`A_C_FISHLAKESTURGEON_01_LG`]   = FishData.A_C_FISHLAKESTURGEON_01_LG[1],
-    [`A_C_FISHLARGEMOUTHBASS_01_LG`] = FishData.A_C_FISHLARGEMOUTHBASS_01_LG[1],
-    [`A_C_FISHLARGEMOUTHBASS_01_MS`] = FishData.A_C_FISHLARGEMOUTHBASS_01_MS[1],
-    [`A_C_FISHLONGNOSEGAR_01_LG`]    = FishData.A_C_FISHLONGNOSEGAR_01_LG[1],
-    [`A_C_FISHMUSKIE_01_LG`]         = FishData.A_C_FISHMUSKIE_01_LG[1],
-    [`A_C_FISHNORTHERNPIKE_01_LG`]   = FishData.A_C_FISHNORTHERNPIKE_01_LG[1],
-    [`A_C_FISHPERCH_01_MS`]          = FishData.A_C_FISHPERCH_01_MS[1],
-    [`A_C_FISHPERCH_01_SM`]          = FishData.A_C_FISHPERCH_01_SM[1],
-    [`A_C_FISHRAINBOWTROUT_01_LG`]   = FishData.A_C_FISHRAINBOWTROUT_01_LG[1],
-    [`A_C_FISHRAINBOWTROUT_01_MS`]   = FishData.A_C_FISHRAINBOWTROUT_01_MS[1],
-    [`A_C_FISHREDFINPICKEREL_01_MS`] = FishData.A_C_FISHREDFINPICKEREL_01_MS[1],
-    [`A_C_FISHREDFINPICKEREL_01_SM`] = FishData.A_C_FISHREDFINPICKEREL_01_SM[1],
-    [`A_C_FISHROCKBASS_01_MS`]       = FishData.A_C_FISHROCKBASS_01_MS[1],
-    [`A_C_FISHROCKBASS_01_SM`]       = FishData.A_C_FISHROCKBASS_01_SM[1],
-    [`A_C_FISHSALMONSOCKEYE_01_LG`]  = FishData.A_C_FISHSALMONSOCKEYE_01_LG[1],
-    [`A_C_FISHSALMONSOCKEYE_01_ML`]  = FishData.A_C_FISHSALMONSOCKEYE_01_ML[1],
-    [`A_C_FISHSALMONSOCKEYE_01_MS`]  = FishData.A_C_FISHSALMONSOCKEYE_01_MS[1],
-    [`A_C_FISHSMALLMOUTHBASS_01_LG`] = FishData.A_C_FISHSMALLMOUTHBASS_01_LG[1],
-    [`A_C_FISHSMALLMOUTHBASS_01_MS`] = FishData.A_C_FISHSMALLMOUTHBASS_01_MS[1],
-}
-
-local fishTextures = {
-    [`A_C_FISHBLUEGIL_01_MS`]        = FishData.A_C_FISHBLUEGIL_01_MS[2],
-    [`A_C_FISHBLUEGIL_01_SM`]        = FishData.A_C_FISHBLUEGIL_01_SM[2],
-    [`A_C_FISHBULLHEADCAT_01_MS`]    = FishData.A_C_FISHBULLHEADCAT_01_MS[2],
-    [`A_C_FISHBULLHEADCAT_01_SM`]    = FishData.A_C_FISHBULLHEADCAT_01_SM[2],
-    [`A_C_FISHCHAINPICKEREL_01_MS`]  = FishData.A_C_FISHCHAINPICKEREL_01_MS[2],
-    [`A_C_FISHCHAINPICKEREL_01_SM`]  = FishData.A_C_FISHCHAINPICKEREL_01_SM[2],
-    [`A_C_FISHCHANNELCATFISH_01_LG`] = FishData.A_C_FISHCHANNELCATFISH_01_LG[2],
-    [`A_C_FISHCHANNELCATFISH_01_XL`] = FishData.A_C_FISHCHANNELCATFISH_01_XL[2],
-    [`A_C_FISHLAKESTURGEON_01_LG`]   = FishData.A_C_FISHLAKESTURGEON_01_LG[2],
-    [`A_C_FISHLARGEMOUTHBASS_01_LG`] = FishData.A_C_FISHLARGEMOUTHBASS_01_LG[2],
-    [`A_C_FISHLARGEMOUTHBASS_01_MS`] = FishData.A_C_FISHLARGEMOUTHBASS_01_MS[2],
-    [`A_C_FISHLONGNOSEGAR_01_LG`]    = FishData.A_C_FISHLONGNOSEGAR_01_LG[2],
-    [`A_C_FISHMUSKIE_01_LG`]         = FishData.A_C_FISHMUSKIE_01_LG[2],
-    [`A_C_FISHNORTHERNPIKE_01_LG`]   = FishData.A_C_FISHNORTHERNPIKE_01_LG[2],
-    [`A_C_FISHPERCH_01_MS`]          = FishData.A_C_FISHPERCH_01_MS[2],
-    [`A_C_FISHPERCH_01_SM`]          = FishData.A_C_FISHPERCH_01_SM[2],
-    [`A_C_FISHRAINBOWTROUT_01_LG`]   = FishData.A_C_FISHRAINBOWTROUT_01_LG[2],
-    [`A_C_FISHRAINBOWTROUT_01_MS`]   = FishData.A_C_FISHRAINBOWTROUT_01_MS[2],
-    [`A_C_FISHREDFINPICKEREL_01_MS`] = FishData.A_C_FISHREDFINPICKEREL_01_MS[2],
-    [`A_C_FISHREDFINPICKEREL_01_SM`] = FishData.A_C_FISHREDFINPICKEREL_01_SM[2],
-    [`A_C_FISHROCKBASS_01_MS`]       = FishData.A_C_FISHROCKBASS_01_MS[2],
-    [`A_C_FISHROCKBASS_01_SM`]       = FishData.A_C_FISHROCKBASS_01_SM[2],
-    [`A_C_FISHSALMONSOCKEYE_01_LG`]  = FishData.A_C_FISHSALMONSOCKEYE_01_LG[2],
-    [`A_C_FISHSALMONSOCKEYE_01_ML`]  = FishData.A_C_FISHSALMONSOCKEYE_01_ML[2],
-    [`A_C_FISHSALMONSOCKEYE_01_MS`]  = FishData.A_C_FISHSALMONSOCKEYE_01_MS[2],
-    [`A_C_FISHSMALLMOUTHBASS_01_LG`] = FishData.A_C_FISHSMALLMOUTHBASS_01_LG[2],
-    [`A_C_FISHSMALLMOUTHBASS_01_MS`] = FishData.A_C_FISHSMALLMOUTHBASS_01_MS[2],
-}
 
 local playersFishing = {}
 
 CreateThread(function()
-    for _, item in pairs(Baits) do
+    for _, item in ipairs(Baits) do
         exports.vorp_inventory:registerUsableItem(item, function(data)
             playersFishing[data.source] = true
             exports.vorp_inventory:closeInventory(data.source)
             exports.vorp_inventory:subItemById(data.source, data.item.id)
             TriggerClientEvent("vorp_fishing:UseBait", data.source, item)
-        end)
+        end, GetCurrentResourceName())
     end
 end)
 
@@ -120,8 +61,11 @@ RegisterServerEvent('vorp_fishing:FishToInventory', function(netid)
     local entity = NetworkGetEntityFromNetworkId(netid)
     local fishModel = GetEntityModel(entity)
     local fish = fishEntity[fishModel]
-    local fish_name = fishNames[fishModel]
-    local fish_texture = fishTextures[fishModel]
+    if not fish then return print("Fish model not found in table fishEntity", fishModel) end
+    local fish_name = fish.name
+    if not fish_name then return print("Fish name not found in table fishNames", fishModel) end
+    local fish_texture = fish.texture
+    if not fish_texture then return print("Fish texture not found in table fishTextures", fishModel) end
 
     exports.vorp_inventory:addItem(_source, fish, 1)
     VORPcore.NotifyAvanced(_source, T.YourGot .. " " .. fish_name, "inventory_items", fish_texture, "COLOR_PURE_WHITE", 4000)
@@ -139,9 +83,9 @@ RegisterServerEvent('vorp_fishing:discord', function(fishModel, fishWeight, stat
     local Character = VORPcore.getUser(_source).getUsedCharacter
 
     local fish = fishEntity[fishModel]
-    if not fish then return end
+    if not fish then return print("Fish model not found in table fishEntity", fishModel) end
 
-    local fish_name = fishNames[fishModel]
+    local fish_name = fish.name
     local fish_weight = string.format("%.2f%%", (fishWeight * 54.25))
     local webhook = "" -- link here for webhook
     local botname = Config.DiscordBotName
